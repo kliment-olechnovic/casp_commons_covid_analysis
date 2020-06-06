eb347cd0e57a1ffde468d672ccdb667c963d5e6f
kliment-olechnovic <kliment.olechnovic@gmail.com>
2020-06-06 16:35:26 +0300

    Added sets of unique models.

---
+++ scripts/collect_unique_models_for_target.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+TARGETNAME="$1"
+
+if [ -z "$TARGETNAME" ]
+then
+	echo >&2 "Error: missing target name"
+	exit 1
+fi
+
+readonly TMPLDIR=$(mktemp -d)
+trap "rm -r $TMPLDIR" EXIT
+
+SCORENAME="cadscores"
+
+{
+echo "model1 model2 score"
+
+cat "./output/${SCORENAME}/all_${SCORENAME}" \
+| egrep "^${TARGETNAME}TS"
+} \
+> "$TMPLDIR/scores"
+
+cd "$TMPLDIR"
+
+R --vanilla > /dev/null << 'EOF'
+
+dt_scores=read.table("scores", header=TRUE, stringsAsFactors=FALSE);
+
+models=sort(union(dt_scores$model1, dt_scores$model2));
+model_representatives=c();
+
+for(model in models)
+{
+	sdt_scores1=dt_scores[which(dt_scores$model1==model),];
+	sdt_scores1=sdt_scores1[which(sdt_scores1$score>0.90),];
+	sdt_models1=sdt_scores1$model2;
+	
+	sdt_scores2=dt_scores[which(dt_scores$model2==model),];
+	sdt_scores2=sdt_scores2[which(sdt_scores2$score>0.90),];
+	sdt_models2=sdt_scores2$model1;
+	
+	model_representatives=c(model_representatives, sort(intersect(sdt_models1, sdt_models2))[1]);
+}
+
+model_representatives=intersect(model_representatives, model_representatives);
+
+write(model_representatives, file="result", ncolumns=1);
+
+EOF
+
+cd - &> /dev/null
+
+OUTDIR="./output/sets_of_unique_models"
+
+mkdir -p "$OUTDIR"
+
+cat "$TMPLDIR/result" > "${OUTDIR}/${TARGETNAME}.txt"
+
--- scripts/workflow.bash
+++ scripts/workflow.bash
@@ -1,33 +1,43 @@
 #!/bin/bash
 
+readonly TMPLDIR=$(mktemp -d)
+trap "rm -r $TMPLDIR" EXIT
+
+cat > "$TMPLDIR/targets" << 'EOF'
+C1901
+C1902
+C1903
+C1904
+C1905
+C1906
+C1907
+C1908
+C1909
+C1910
+C1901d1
+C1901d2
+C1901d3
+C1902d1
+C1902d2
+C1902d3
+C1903d1
+C1903d2
+C1904d1
+C1904d2
+C1904d3
+C1905d1
+C1905d2
+C1906d1
+C1906d2
+EOF
+
 for SCORENAME in cadscores lddts
 do
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1901
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1902
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1903
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1904
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1905
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1906
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1907
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1908
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1909
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1910
+	cat "$TMPLDIR/targets" \
+	| xargs -L 1 ./scripts/collect_unique_models_for_target.bash
 	
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1901d1
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1901d2
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1901d3
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1902d1
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1902d2
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1902d3
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1903d1
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1903d2
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1904d1
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1904d2
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1904d3
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1905d1
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1905d2
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1906d1
-	./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all C1906d2
+	cat "$TMPLDIR/targets" \
+	| xargs -L 1 ./scripts/calculate_consensus_scores_for_target.bash "$SCORENAME" all
 	
 	./scripts/summarize_notable_models.bash "$SCORENAME" domain
 	./scripts/summarize_notable_models.bash "$SCORENAME" full
